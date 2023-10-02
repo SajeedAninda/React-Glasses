500d5c171b3b903f44db0454be4a3a6ecebeaa7b
SajeedAninda <sajaninda@gmail.com>
2023-10-02 13:49:25 +0600

    Added Sign Out

--- src/Components/AuthProvider/AuthProvider.jsx
+++ src/Components/AuthProvider/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
-import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { app } from '../Firebase/firebase.config';
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
@@ -8,18 +8,27 @@ export let AuthContext = createContext(null);
 
 
 const AuthProvider = ({ children }) => {
+    let [currentUser, setCurrentUser] = useState(null);
 
-    let googleLogin=()=>{
-       return signInWithPopup(auth, provider);
+    let googleLogin = () => {
+        return signInWithPopup(auth, provider);
     }
 
-    let register=(email,password)=>{
+    let register = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
-    let login=(email,password)=>{
+    let login = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password)
     }
+    let logOut = () => {
+        return signOut(auth);
+    }
 
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+        });
+    }, [])
 
 
 
@@ -28,7 +37,9 @@ const AuthProvider = ({ children }) => {
     let authentication = {
         googleLogin,
         register,
-        login
+        login,
+        logOut,
+        currentUser
     }
     return (
         <AuthContext.Provider value={authentication}>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
 const Menus = () => {
     const links = ['products', 'about', 'contact', 'blog'];
@@ -13,6 +15,17 @@ const Menus = () => {
     )
 }
 const NavBar = () => {
+    let { logOut, currentUser } = useContext(AuthContext);
+
+    // let handleLogOut = () => {
+    //     logOut()
+    //         .then(() => {
+    //             // Sign-out successful.
+    //         }).catch((error) => {
+    //             console.log(error);
+    //         });
+    // }
+
     const user = false;
     return (
         <>
@@ -39,19 +52,19 @@ const NavBar = () => {
                 <div className="navbar-end">
 
                     {
-                        user?.email ? <div className="dropdown dropdown-end">
+                        currentUser?.email ? <div className="dropdown dropdown-end">
                             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                                 <div className="w-10 rounded-full">
-                                    <img src="https://i.ibb.co/y0yrnYQ/1681283571946.jpg" />
+                                    <img src={currentUser.photoURL} />
                                 </div>
                             </label>
                             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                                 <li>
-                                    <button className="btn btn-sm  btn-ghost">Farhan</button>
+                                    <button className="btn btn-sm  btn-ghost">{currentUser.email}</button>
 
                                 </li>
                                 <li>
-                                    <button className="btn btn-sm  btn-ghost">Logout</button>
+                                    <button onClick={logOut} className="btn btn-sm  btn-ghost">Logout</button>
 
                                 </li>
                             </ul>
